68bdf8ee93a8706c2da61758cd04c3078c9d5d22
Irene Chang <irene84111@gmail.com>
2017-03-10 10:11:21 +0800

    touched up

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,16 +5,12 @@
     var $window = $(window),
         $body = $('body');
 
-    // Disable animations/transitions until the page has loaded
-
-    $(function() {
-        $('.explore').bind('click', function(event) {
-            var $anchor = $(this);
-            $('html, body').stop().animate({
-                scrollTop: $($anchor.attr('href')).offset().top
-            }, 1500, 'easeInOutExpo');
-            event.preventDefault();
-        });
+    $('.explore').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
     });
   });
 })(jQuery);
